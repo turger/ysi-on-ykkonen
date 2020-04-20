be54789fa245fb1285c55a5a5e3fe3bb248f0465
Taru Airola <taru.airola@wunderdog.fi>
2020-04-20 11:30:18 +0300

    Change emojis back to images because pi didn't like unicode versions

--- src/Emoji.js
+++ src/Emoji.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import emojione from 'emojione'
 
-const Emoji = ({name}) => <div className="Emoji">{emojione.shortnameToUnicode(name)}</div>
+const Emoji = ({name}) => <div className="Emoji" dangerouslySetInnerHTML={{__html: emojione.shortnameToImage(name)}}/>
 
 Emoji.propTypes = {name: PropTypes.string.isRequired}
 
